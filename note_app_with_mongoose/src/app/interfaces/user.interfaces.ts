d0174c33b09b6e7328fa36ba0df7da36b4280908
rubelrana123 <rubelranatpi707458@gmail.com>
2025-06-21 01:02:05 +0600

    add validator

--- note_app_with_mongoose/src/app/interfaces/user.interfaces.ts
+++ note_app_with_mongoose/src/app/interfaces/user.interfaces.ts
@@ -1,7 +1,8 @@
 export interface IUSer {
     firstName : string,
     lastName : string,
+    age : number,
     email : string,
     password : string,
-    role : "user" | "admin"
+    role : 'USER'|'ADMIN'| 'SUPER-ADMIN'
 }
--- note_app_with_mongoose/src/app/models/users.model.ts
+++ note_app_with_mongoose/src/app/models/users.model.ts
@@ -1,23 +1,44 @@
 import { model, Schema } from "mongoose";
 import { IUSer } from "../interfaces/user.interfaces";
-
+import validator from "validator";
   const userSchema  = new Schema<IUSer>({
     firstName : {
         type : String,
-        required : true,
-        trim : true
+        required : [true, "first name missiong"],
+        trim : true,
+        minlength : [5, "{VALUE}  only need atleast 5 length"],
+        maxlength : [15, "max length allow 15"]
 
     },
         lastName : {
         type : String,
         required : true,
-        trim : true
+        trim : true,
+        minlength : [5, "{VALUE} only need atleast 5 length"],
+        maxlength : [15, "max length allow 15"]
 
     },
     email : {
         type : String, 
         required : true,
-        trim : true
+        unique : true,
+        lowercase : true,
+        trim : true,
+        //approach 1
+       validate : [ validator.isEmail, "invalid email address"]
+        //approach 2
+        // validate: {
+//     validator: function (v: string) {
+//       // Basic email pattern
+//       return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+//     },
+//     message: (props: any) => `${props.value} is not a valid email address!`
+//   }
+        //approach 2
+    //    match: [
+    //   /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    //   "Please fill a valid email address",
+    // ],
     },
     password : {
         type : String,
@@ -25,8 +46,18 @@ import { IUSer } from "../interfaces/user.interfaces";
     },
     role : {
         type : String,
-        enum : ['user','admin'],
-        default : 'user'
+        uppercase : true,
+        enum : {
+             values : ['USER','ADMIN', 'SUPER-ADMIN'],
+             message : "type a valid role"
+        },
+        default : 'USER'
+    },
+    age : {
+        type : Number,
+        required : true,
+        min : 18,
+        max : 60
     }
 
 })
