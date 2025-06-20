cb82f3ed196d3aed158ae481a04c33fd6759a41e
rubelrana123 <rubelranatpi707458@gmail.com>
2025-06-20 20:54:52 +0600

    add delete, update, create,find, versiokeys,timestamps

--- note_app_with_mongoose/src/app/app.ts
+++ note_app_with_mongoose/src/app/app.ts
@@ -11,9 +11,13 @@ const noteSchema = new Schema({
     tags : {
         label : {type : String, required : true}, 
         color : {type : String, default : "gray"}
-
 }
-});
+},
+{
+    versionKey : false,
+    timestamps : true
+},
+);
 const Note = model("Note", noteSchema)
 app.get("/notes", async(req : Request, res : Response) => {
     const notes = await Note.find({})
@@ -26,12 +30,32 @@ app.get("/notes", async(req : Request, res : Response) => {
 })
 app.get("/notes/:noteId", async(req : Request, res : Response) => {
     const id = req.params.noteId;
-    // const notes = await Note.findById(id);
-    const notes = await Note.findOne(_id = id);
-
+    const notes = await Note.findById(id);
+    // const notes = await Note.findOne(_id = id);
     res.status(201).json({
         success : true, 
         message : "get a note successfully",
+        notes
+    })
+    
+});
+app.patch("/notes/:noteId", async(req : Request, res : Response) => {
+    const noteId = req.params.noteId;
+    const body = req.body;
+    //approach 1
+      const notes = await Note.findByIdAndUpdate(noteId,body, {new : true})
+      //apporoach 2
+    //   const notes = await Note.updat eOne({_id : noteId},body, {new : true})
+
+      //apporach 3
+//  const notes = await Note.findOneAndUpdate(
+//   { _id: id }, // Filter to find the document
+//   { $set: { title: 'Learn Typescript', tags : {label : "Basic"} } }, // Update operations
+//   { new: true } // Options: return the modified document
+// );
+    res.status(201).json({
+        success : true, 
+        message : " a note update successfully",
         notes
     })
     
@@ -55,6 +79,36 @@ app.post("/notes/create-note", async(req : Request, res : Response) => {
     })
     
 })
+app.delete("/notes/:noteId", async(req : Request, res : Response) => {
+    const noteId = req.params.noteId;
+    // apporaoach 1
+          const notes = await Note.findOneAndDelete({_id : noteId});
+      //apporoach 2
+    //   const notes = await Note.deleteOneOne({_id : noteId},body, {new : true})
+    //approach another
+    //  Note.deleteOne({ _id: noteId})
+    //   .then(result => {
+    //     if (result.deletedCount > 0) {
+    //        res.status(201).json({
+    //     success : true, 
+    //     message : " a note delete successfully",
+        
+    // })
+    //     } else {
+    //       console.log('note not found');
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.error('Error deleting user:', error);
+    //   });
+
+        res.status(201).json({
+        success : true, 
+        message : " a note delete successfully",
+        notes
+    })
+  })
+ 
 app.get("/", (req : Request, res : Response) => {
     res.send("welcome to the note app")
 })
